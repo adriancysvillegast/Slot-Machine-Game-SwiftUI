58e98298e9a321176ae9892a4e6b4a715a2ac679
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-10-24 18:28:52 -0400

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Adriancys Jesus Villegas Toro on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
